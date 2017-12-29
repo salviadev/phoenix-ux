fcd230416327e699101ac90200246602991b159d
Anthony Lesage <anles@salviadeveloppement.com>
2017-12-29 17:09:47 +0100

    fini pages champs et forms, ajout page tableau-simple

---
+++ src/ergo/ergo/controller/tableau-simple.ts
@@ -0,0 +1,13 @@
+/// <reference types="phoenix-cli" />
+
+namespace Ergo {
+    const _p = Phoenix, _customData = _p.customData, _dom = _p.dom, _dsPlugin = _p.DatasetPlugin, _link = _p.link, _data = _p.data;
+    class tableauSimple extends Phoenix.ui.FormController {
+        public beforeSetModel(data) {
+        }
+        public onModelChanged(action, model, form) {      
+        }
+    }
+    _customData.register('ergo.tableau-simple.controller', new tableauSimple());
+}
+
