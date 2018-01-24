32ac9159c5321f9e793853001a4244eaa6b36bf7
Anthony Lesage <anles@salviadeveloppement.com>
2018-01-24 11:03:28 +0100

    tile bug + detail-inside-tab choose + some modif

---
+++ src/ergo/ergo/controllers/detail-inside-tab.ts
@@ -0,0 +1,21 @@
+/// <reference types="phoenix-cli" />
+
+namespace Ergo {
+    const _p = Phoenix, _customData = _p.customData, _dom = _p.dom, _dsPlugin = _p.DatasetPlugin, _link = _p.link, _data = _p.data;
+    class tableauSimple extends Phoenix.ui.FormController {
+        public beforeSetModel(data) {
+        }
+        public onModelChanged(action, model, form) {
+            switch (action.property) {
+                case '$links.$beforeEdit':
+                    action.actionParams.$states.Type.isReadOnly = true;  
+                    action.actionParams.$states.Libelle.isReadOnly = true;
+                    break;
+                case '$links.$afterUpdateChanges':
+                    break;
+            }
+        }
+    }
+    _customData.register('ergo.detail-inside-tab.controller', new tableauSimple());
+}
+
---
+++ src/shared/js/summary/tile.ts
@@ -0,0 +1,33 @@
+/// <reference types="phoenix-cli" />
+namespace PhoenixLib {
+    const _p = Phoenix;
+    let item_render = {
+        render: function (parent, item, map, index) {
+            map[item.id] = item.id;
+            const html: string[] = [];
+            html.push('<div id="' + item.id + '" class="accession-tile-div accession-tile-color-one"><div><h4 class="accession-tile-title">' + item.title.toUpperCase() + '</h4>');
+            for (var i = 0; i < item.infos.length; i++) {
+
+                if (item.infos[i]) {
+                    if (item.infos[i].title) {
+                        html.push('<div><p>' + item.infos[i].title + ' : ');
+                        if (item.infos[i].value)
+                            html.push(item.infos[i].value);
+                        html.push('</p>');
+                    }
+                    else {
+                        html.push('<div><p>' + item.infos[i].value + '</p>');
+                    }
+                }
+            }
+            parent.appendChild($(html.join('')).get(0));
+
+
+
+        },
+        click(event, map) {
+        }
+
+    }
+    _p.customData.register("accession-resume-lot-render", item_render);
+}
